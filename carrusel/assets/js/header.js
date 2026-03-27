619563327cfb0c3a862657e98ace95c727bf3da1
Raul Fernando Ruiz Melendez <Raul Ruiz>
2026-03-27 10:49:55 -0600

    [MODIFICAR]Modificacion del header y adicion de mapa con geolocalizacion

---
+++ carrusel/assets/js/header.js
@@ -0,0 +1,35 @@
+const header = document.querySelector("header");
+
+header.innerHTML = `<div class="logo_header">
+            <a id="logo_ir_a_inicio" href="/index.html" >
+            <img src="./img/recursos/SVG/Recurso 1.svg" alt="">
+            </a>
+        </div>
+        <nav class="navegador_header">
+            <div id="ir_a_inicio" class="activo">
+                <a href="index.html">HOME</a>
+            </div>
+            <div class="enlaces" id="mis_reseñas_usuario">
+                <a href="">RESEÑAS</a>
+            </div>
+            <div class="enlaces">
+                <a href="">MI CUENTA</a>
+            </div>
+        </nav>`
+
+const mis_reseñas_usuario = document.querySelector("#mis_reseñas_usuario");
+
+mis_reseñas_usuario.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const token = sessionStorage.getItem("token_sesion");
+    console.log("Token actual:", token);
+
+    if (!token || token === "null" || token === "undefined") {
+        window.location.href = "error.html";
+    } else {
+        window.location.href = "reseñas.html";
+    }
+});
+
+
