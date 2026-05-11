547a13752584c694b2568c217318b95fe823929c
RaulFrenandoRuizMel <raulfernandoruizmelendez@gmail.com>
2026-05-11 10:27:26 -0600

    [MODIFICAR]Modificaciones generales

--- proyecto_internacionalizacion/assets/js/footer.js
+++ proyecto_internacionalizacion/assets/js/footer.js
@@ -4,9 +4,10 @@ footer.innerHTML = `  <div class="logo_sociales">
             <img class="logo" src="./assets/images/logotipo_uacj.png" alt="">
             <p class="texto"> Visitanos en:</p>
             <div class="redes_sociales">
-                <img src="./assets/images/youtube-logo-icon-free-png.webp" alt="">
-                <img src="./assets/images/FB-Icon-Facebook-Logo-Grey-Circle-PNG.png" alt="">
-                <img src="./assets/images/sankt-petersburg-russia-24-08-2023-twitter-new-logo-twitter-icons-twitter-x-logo-free-png.webp"alt="">
+                <a href="https://www.youtube.com/@internacionalizacionuacj1261"><img src="./assets/images/youtube-logo-icon-free-png.webp" alt=""></a>
+                <a href="https://www.facebook.com/internacionalizacion.uacj/?locale=es_LA"><img src="./assets/images/FB-Icon-Facebook-Logo-Grey-Circle-PNG.png" alt=""></a>
+                <a href="https://x.com/somosuacj"><img src="./assets/images/sankt-petersburg-russia-24-08-2023-twitter-new-logo-twitter-icons-twitter-x-logo-free-png.webp"alt=""></a>
+                
             </div>
         </div>
         <div class="sobre_nosotros">
